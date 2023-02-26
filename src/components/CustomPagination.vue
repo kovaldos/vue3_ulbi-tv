3ca12da25ceb74c23d744a1ad319c601a0900459
kovaldos <ya.kovalev@ya-kovalev.ru>
2023-02-26 15:24:32 +0300

    feat: add pagination SFC

---
+++ src/components/CustomPagination.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="pagination">
+    <div class="container">
+      <ul class="pagination__list">
+        <li
+          class="pagination__list-item"
+          v-for="pageNumber in totalPages"
+          :key="pageNumber"
+        >
+          <custom-btn
+            class="pagination__btn"
+            :class="{
+              'pagination__btn--current': currentPage === pageNumber,
+            }"
+            @click="changePageAndPageNumber(pageNumber)"
+            >{{ pageNumber }}</custom-btn
+          >
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      currentPage: 1,
+    };
+  },
+  props: {
+    totalPages: {
+      type: Number,
+      required: true,
+    },
+  },
+  methods: {
+    changePageAndPageNumber(pageNumber) {
+      this.currentPage = pageNumber;
+      this.$emit("changePage", pageNumber);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
