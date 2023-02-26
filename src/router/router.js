9390c3a438e96315fb5def2bdd4e1999f5bbe91e
kovaldos <ya.kovalev@ya-kovalev.ru>
2023-02-26 15:23:01 +0300

    feat: add router

--- src/router/router.js
+++ src/router/router.js
@@ -3,12 +3,16 @@ import MainPage from "@/pages/MainPage.vue";
 import PostsPage from "@/pages/PostsPage.vue";
 import AboutPage from "@/pages/AboutPage.vue";
 import PostPage from "@/pages/PostPage.vue";
+import PostsPageWithStore from "@/pages/PostsPageWithStore.vue";
+import PostsPageCompositionAPI from "@/pages/PostsPageCompositionAPI.vue";
 
 const routes = [
   { path: "/", component: MainPage },
   { path: "/posts", component: PostsPage },
   { path: "/about", component: AboutPage },
   { path: "/posts/:id", component: PostPage },
+  { path: "/store", component: PostsPageWithStore },
+  { path: "/composition", component: PostsPageCompositionAPI },
 ];
 
 const router = createRouter({
