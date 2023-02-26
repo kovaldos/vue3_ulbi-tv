05fb41cf7e5bbf2633c97abedc88d1e3a2688959
kovaldos <ya.kovalev@ya-kovalev.ru>
2023-02-26 15:43:03 +0300

    feat: add header

---
+++ src/components/MainNav.vue
@@ -0,0 +1,49 @@
+<template>
+  <nav class="main-nav greetings__nav">
+    <ul class="main-nav__list greetings__nav-list">
+      <li class="main-nav__list-item">
+        <router-link to="/posts" class="link main-nav__link">Posts</router-link>
+      </li>
+      <li class="main-nav__list-item">
+        <router-link to="/store" class="link main-nav__link">Store</router-link>
+      </li>
+      <li class="main-nav__list-item">
+        <router-link to="/composition" class="link main-nav__link"
+          >Composition API</router-link
+        >
+      </li>
+      <li class="main-nav__list-item">
+        <router-link to="/about" class="link main-nav__link">About</router-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "MainNav",
+};
+</script>
+
+<style scoped lang="scss">
+@import "@/scss/style.scss";
+@import "@/scss/common/link.scss";
+.main-nav {
+  &__list {
+    list-style: none;
+    display: flex;
+    &-item {
+      &:not(:last-of-type) {
+        margin-right: 1rem;
+      }
+    }
+  }
+  &.router-link-active.router-link-exact-active {
+    @include hover-focus {
+      &::after {
+        transform: scale(1);
+      }
+    }
+  }
+}
+</style>
---
+++ src/components/VueHeader.vue
@@ -0,0 +1,48 @@
+<template>
+  <header class="header">
+    <div class="container">
+      <div class="header__logo" v-if="$route.path === '/'">
+        <span>
+          <img src="@/assets/img/logo.svg" alt="App logo" />
+        </span>
+      </div>
+      <div class="header__logo" v-else>
+        <router-link to="/">
+          <img src="@/assets/img/logo.svg" alt="App logo" />
+        </router-link>
+      </div>
+      <MainNav />
+    </div>
+  </header>
+</template>
+
+<script>
+import MainNav from "@/components/MainNav.vue";
+
+export default {
+  name: "VueHeader",
+  components: { MainNav },
+};
+</script>
+
+<style scoped lang="scss">
+@import "@/scss/variables.scss";
+.header {
+  z-index: 5;
+  position: fixed;
+  width: 100%;
+  min-height: 90px;
+  padding: 1rem;
+  background-color: $color-dark-orange;
+  & .container {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+  &__logo {
+    flex-shrink: 0;
+    width: 35px;
+    height: 35px;
+  }
+}
+</style>
