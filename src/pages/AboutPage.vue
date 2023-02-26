6a8c15d158d377050f1322258aa479121c358c92
kovaldos <ya.kovalev@ya-kovalev.ru>
2023-02-26 15:23:18 +0300

    feat: add about page

---
+++ src/pages/AboutPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <section class="section">
+    <div class="container">
+      <h1 class="title title--h1">
+        This application was created while learning the Vue 3 framework.
+      </h1>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
